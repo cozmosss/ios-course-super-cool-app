3ddca84119c1cb1013e9414fc937937595d1f167
Mark Kramer <markkramer@Marks-MacBook-Pro.local>
2016-01-12 18:05:49 -0500

    How many fingers

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtGuessedAmt: UITextField!
+    
+    @IBOutlet var lblErrorTxt: UILabel!
+    
+    @IBAction func btnActualAmount(sender: AnyObject) {
+        
+        var actualAmt = arc4random_uniform(5) + 1
+        
+        println(actualAmt)
+        
+        if txtGuessedAmt.text.toInt() < 0 || txtGuessedAmt.text.toInt() > 6 {
+            lblErrorTxt.text = "Please enter a valid number"
+        
+        } else {
+            if txtGuessedAmt.text.toInt() == Int(actualAmt) {
+                lblErrorTxt.text = "Congratulations, you guessed right"
+                
+            } else {
+                lblErrorTxt.text = "Please try again"
+                
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
